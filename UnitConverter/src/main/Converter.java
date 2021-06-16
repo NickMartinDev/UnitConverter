e96cccf0e8697883e12d6f53e04da88fbea93a15
Nicholas Martin <nmartin12800@gmail.com>
2021-06-16 10:05:23 -0400

    Fixed unit converter to continuously ask for input

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -4,75 +4,80 @@ import java.util.Scanner;
 public class Converter {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please select an option:");
-		System.out.println("1) Metric to imperial");
-		System.out.println("2) Imperial to metric");
-		System.out.println("3) Quit");
-		String line = scanner.nextLine();
-		switch (line) {
-		case "1":
+		boolean quit = false;
+		while(!quit) {
 			System.out.println("Please select an option:");
-			System.out.println("1) Centimeters to inches");
-			System.out.println("2) Meters to feet");
-			System.out.println("3) Kilometers to miles");
-			line = scanner.nextLine();
-			switch(line)
-			{
+			System.out.println("1) Metric to imperial");
+			System.out.println("2) Imperial to metric");
+			System.out.println("3) Quit");
+			String line = scanner.nextLine();
+			switch (line) {
 			case "1":
-				System.out.println("Type in the number to convert:");
+				System.out.println("Please select an option:");
+				System.out.println("1) Centimeters to inches");
+				System.out.println("2) Meters to feet");
+				System.out.println("3) Kilometers to miles");
 				line = scanner.nextLine();
-				System.out.println(line + " centimeters is " + convertFromCentimetersToInches(Float.parseFloat(line)) + " inches");
+				switch(line)
+				{
+				case "1":
+					System.out.println("Type in the number to convert:");
+					line = scanner.nextLine();
+					System.out.println(line + " centimeters is " + convertFromCentimetersToInches(Float.parseFloat(line)) + " inches");
+					break;
+				case "2":
+					System.out.println("Type in the number to convert:");
+					line = scanner.nextLine();
+					System.out.println(line + " meters is " + convertFromMetersToFeet(Float.parseFloat(line)) + " feet");
+					break;
+				case "3":
+					System.out.println("Type in the number to convert:");
+					line = scanner.nextLine();
+					System.out.println(line + " kilometers is " + convertFromKilometersToMiles(Float.parseFloat(line)) + " miles");
+					break;
+				default:
+					System.out.println("Invalid Input");
+					break;
+				}
 				break;
 			case "2":
-				System.out.println("Type in the number to convert:");
+				System.out.println("Please select an option:");
+				System.out.println("1) Inches to centimeters");
+				System.out.println("2) Feet to meters");
+				System.out.println("3) Miles to kilometers");
 				line = scanner.nextLine();
-				System.out.println(line + " meters is " + convertFromMetersToFeet(Float.parseFloat(line)) + " feet");
+				switch(line)
+				{
+				case "1":
+					System.out.println("Type in the number to convert:");
+					line = scanner.nextLine();
+					System.out.println(line + " inches is " + convertFromCentimetersToInches(Float.parseFloat(line)) + " centimeters");
+					break;
+				case "2":
+					System.out.println("Type in the number to convert:");
+					line = scanner.nextLine();
+					System.out.println(line + " feet is " + convertFromMetersToFeet(Float.parseFloat(line)) + " meters");
+					break;
+				case "3":
+					System.out.println("Type in the number to convert:");
+					line = scanner.nextLine();
+					System.out.println(line + " miles is " + convertFromKilometersToMiles(Float.parseFloat(line)) + " kilometers");
+					break; 
+				default:
+					System.out.println("Invalid Input");
+					break;
+				}
 				break;
 			case "3":
-				System.out.println("Type in the number to convert:");
-				line = scanner.nextLine();
-				System.out.println(line + " kilometers is " + convertFromKilometersToMiles(Float.parseFloat(line)) + " miles");
+				System.out.println("Quitting");
+				quit = true;
 				break;
 			default:
 				System.out.println("Invalid Input");
 				break;
 			}
-			break;
-		case "2":
-			System.out.println("Please select an option:");
-			System.out.println("1) Inches to centimeters");
-			System.out.println("2) Feet to meters");
-			System.out.println("3) Miles to kilometers");
-			line = scanner.nextLine();
-			switch(line)
-			{
-			case "1":
-				System.out.println("Type in the number to convert:");
-				line = scanner.nextLine();
-				System.out.println(line + " inches is " + convertFromCentimetersToInches(Float.parseFloat(line)) + " centimeters");
-				break;
-			case "2":
-				System.out.println("Type in the number to convert:");
-				line = scanner.nextLine();
-				System.out.println(line + " feet is " + convertFromMetersToFeet(Float.parseFloat(line)) + " meters");
-				break;
-			case "3":
-				System.out.println("Type in the number to convert:");
-				line = scanner.nextLine();
-				System.out.println(line + " miles is " + convertFromKilometersToMiles(Float.parseFloat(line)) + " kilometers");
-				break; 
-			default:
-				System.out.println("Invalid Input");
-				break;
-			}
-			break;
-		case "3":
-			System.out.println("Quitting");
-			break;
-		default:
-			System.out.println("Invalid Input");
-			break;
 		}
+		
 		 scanner.close();
 	 }
 	public static float convertFromCentimetersToInches(float centimeters) {
